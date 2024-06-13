88cfb1aedc7fd3303fe253c73d6acfaf33825285
홍덕화(Deokhwa Hong) 연구원 HD현대인프라코어 <deokhwa.hong@hd.com>
2024-06-13 17:18:37 +0900

    24.06.13 second commit

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const bottomContainerHeight = 80.0;
 const Color activeCardColour = Color(0xFFEB1555);
@@ -24,18 +25,42 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: ReusableCard(
                     colour: activeCardColour,
+                    cardChild: IconContent(
+                        icon: FontAwesomeIcons.mars, caption: 'MALE'),
                   ),
                 ),
                 Expanded(
-                    child: ReusableCard(
-                  colour: activeCardColour,
-                )),
+                  child: ReusableCard(
+                    colour: activeCardColour,
+                    cardChild: IconContent(
+                        icon: FontAwesomeIcons.venus, caption: 'FEMALE'),
+                  ),
+                ),
               ],
             ),
           ),
           Expanded(
               child: ReusableCard(
             colour: activeCardColour,
+            cardChild: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(
+                  FontAwesomeIcons.mars,
+                  size: 80.0,
+                ),
+                SizedBox(
+                  height: 15.0,
+                ),
+                Text(
+                  'MALE',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    color: Color(0xFF8D8E98),
+                  ),
+                )
+              ],
+            ),
           )),
           Expanded(
             child: Row(
@@ -43,10 +68,48 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                     child: ReusableCard(
                   colour: activeCardColour,
+                  cardChild: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        FontAwesomeIcons.mars,
+                        size: 80.0,
+                      ),
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                      Text(
+                        'MALE',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          color: Color(0xFF8D8E98),
+                        ),
+                      )
+                    ],
+                  ),
                 )),
                 Expanded(
                     child: ReusableCard(
                   colour: activeCardColour,
+                  cardChild: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        FontAwesomeIcons.mars,
+                        size: 80.0,
+                      ),
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                      Text(
+                        'MALE',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          color: Color(0xFF8D8E98),
+                        ),
+                      )
+                    ],
+                  ),
                 )),
               ],
             ),
@@ -63,14 +126,46 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
+class IconContent extends StatelessWidget {
+  const IconContent({required this.icon, required this.caption});
+
+  final IconData icon;
+  final String caption;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          icon,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          caption,
+          style: TextStyle(
+            fontSize: 18.0,
+            color: Color(0xFF8D8E98),
+          ),
+        )
+      ],
+    );
+  }
+}
+
 class ReusableCard extends StatelessWidget {
-  ReusableCard({required this.colour});
+  ReusableCard({required this.colour, required this.cardChild});
 
   final Color colour; // 한번만 입력하고 변경할 수 없다.
+  final Widget cardChild;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardChild,
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         color: Color(0xFF1D1E33),
